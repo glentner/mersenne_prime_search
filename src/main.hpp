78b6972c9b2f89c162fd32fc68fa5bc1235f9aea
Geoffrey Lentner <glentner@purdue.edu>
2024-03-21 21:55:59 -0400

    Update usage and help text

--- src/main.hpp
+++ src/main.hpp
@@ -29,12 +29,12 @@ enum Status {
 
 
 static const std::string APP_USAGE = R"USAGE(Usage:
-  mps [-hv] PVAL [-t NAME] [-f PATH]
+  mps [-hv] [-p] PVAL [-s] [-t NAME] [-f PATH]
   Check if number is Mersenne prime.)USAGE";
 
 
 static const std::string APP_HELP = R"HELP(Usage:
-  mps [-hv] PVAL [-t TEST] [-f PATH] [-s | -p]
+  mps [-hv] [-p] PVAL [-s] [-t NAME] [-f PATH]
   Check if number is Mersenne prime.
 
 Arguments:
@@ -42,7 +42,7 @@ Arguments:
 
 Options:
   -p, --primes            Print primes up to PVAL.
-  -s, --skip-ptest        Suppress initial test on PVAL.
+  -s, --skip-ptest        Suppress initial prime test on PVAL.
   -t, --test        NAME  One of 'tf', 'p1', 'll' (default: 'all').
   -f, --file        PATH  File path for check-pointing.
   -v, --version           Show version and exit.
